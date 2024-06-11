9a6fb0856f07cb01fd84cf6d7bd00487667271b1
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-11 19:17:01 +0530

    Routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createRoot } from 'react-dom/client'
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import CountryDetail from './components/CountryDetail.jsx';
+import Contact from './components/Contact.jsx';
+import Home from './components/Home.jsx';
+import Error from './components/Error.jsx';
 
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/country',
+        element: <CountryDetail />,
+      },
+    ],
   },
-]);
-
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
