6257315e7539a92592233a4740664e30d73e4b11
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-11 13:03:26 +0530

    files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
+import SearchBar from './components/SearchBar'
+import Header from './components/Header'
+import SelectMenu from './components/SelectMenu'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <div className="search-filter-container">
+          <SearchBar />
+          <SelectMenu />
+        </div>
+      </main>
     </>
   )
 }
+
 
 export default App
