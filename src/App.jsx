72013d500c4697334937f4b1dd7de265656ebb57
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-12 20:52:24 +0530

    files

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import Header from './components/Header'
 import { Outlet } from 'react-router-dom'
 
 import './App.css'
+import { useState } from 'react'
 
 const App = () => {
+  const [isDark, setIsDark] = useState(JSON.parse(localStorage.getItem('isDarkMode')))
+
   return (
     <>
-      <Header />
-      <Outlet />
+      <Header theme={[isDark, setIsDark]} />
+      <Outlet context={[isDark, setIsDark]} />
     </>
   )
 }
